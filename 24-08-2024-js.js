bcbd1a2fe4714d407ff0f03b1524ade2bd738e6c
MANIZABAYO-Elie <152684938+MANIZABAYO-Elie@users.noreply.github.com>
2024-08-24 19:11:27 +0200

    file committed

---
+++ 24-08-2024-js.js
@@ -0,0 +1,16 @@
+//
+console.log("hello world")
+//  variables declaration
+// variable is declared using assignment operator =
+let name = "Elie";
+console.log(name);
+// naming  variables
+//variables are named using camelCase
+// you can use digits, letters, dollar sign $
+// or  underscore _
+// must start with letter or
+let fullName = "MANIZABAYO ELIE"
+console.log(fullName);
+
+
+
